aaaddc438495e21219101706dea3d5e20ca78e3f
Lahiru Sandeepa <lahirusandeepa@outlook.com>
2019-12-06 02:05:39 +0530

    Initial Commit till Dog Page

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'dog_model.dart';
+import 'dog_list.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title:'We rate Dogs',
+      theme:ThemeData(brightness: Brightness.dark),
+      home:MyHomePage(title:'We rate Dogs'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+  MyHomePage({Key key,this.title}):super(key:key);
+  final String title;
+  @override 
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+  List<Dog> initialDoggos=[]
+  ..add(Dog('Ruby','Portland, OR, USA','Ruby is a very good girl. Yes: Fetch, loungin\'. No: Dogs who get on furniture.' ))
+  ..add(Dog('Rex', 'Seattle, WA, USA', 'Best in Show 1999'))
+  ..add(Dog('Rod Stewart', 'Prague, CZ','Star good boy on international snooze team.'))
+  ..add(Dog('Herbert', 'Dallas, TX, USA', 'A Very Good Boy'))
+  ..add(Dog('Buddy', 'North Pole, Earth', 'Self proclaimed human lover.'));
+
+
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(
+        title:Text(widget.title),
+        backgroundColor: Colors.black87,
+      ),
+      body:Container(
+        decoration: BoxDecoration(gradient:LinearGradient(begin: Alignment.topRight,end:Alignment.bottomLeft,stops:[0.1,0.5,0.7,0.9],colors: [Colors.indigo[800],Colors.indigo[700],Colors.indigo[600],Colors.indigo[400]],),),
+        // child: DogCard(initialDoggos[2]),
+        child:Center( 
+          child: DogList(initialDoggos),
+        ),
+      ),
+    );
+  }
+}
